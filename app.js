cfe348bd6248ab10d7d18fa074cab4e7252047e7
ezra-salomon <77461048+ezra-salomon@users.noreply.github.com>
2021-07-18 12:06:34 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
-const paragraph = document.getElementsByTagName("p");
 
-function parEdit(){
-    if(paragraph){
-        console.log('it kinda works!');
-    }
+
+
+function randomRGB(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
 }
 
-console.log(paraEdit());
+
+const letters = document.querySelectorAll(".letter");
+
+setInterval(function(){
+    for (let letter of letters){
+        letter.style.color = randomRGB();
+    }
+}, 1000)
+
+
+
+
+
+const form = document.querySelector("#logoform");
+const nameInput = document.querySelector('input[name="name"]');
+const colorInput = document.querySelector('input[name="color"]');
+const fontSizeInput = document.querySelector('input[name="fontsize"]');
+const results = document.querySelector('#results');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    const newLogo = makeLogo(
+        nameInput.value, 
+        colorInput.value, 
+        fontSizeInput.value
+    );
+
+    results.appendChild(newLogo);
+    
+});
+
+function makeLogo(name, color, size) {
+    const logo = document.createElement('h2');
+
+    logo.innerText = name;
+    logo.style.color = color;
+    logo.style.fontSize = size + 'px';
+    return logo;
+}
+
+
+
+
+
+
+
